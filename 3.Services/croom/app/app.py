5840a164c64af4c22ee159bbbc8f12dff556f1da
오소진 <jaisonoh@naver.com>
2017-02-12 15:30:33 +0900

    Implement background thread using SocketIO

--- 3.Services/croom/app/app.py
+++ 3.Services/croom/app/app.py
@@ -2,16 +2,20 @@ import os
 from flask import Flask, request, render_template, session, flash, redirect, \
     url_for, jsonify
 
+from flask_socketio import SocketIO, emit, disconnect
 from celery import Celery
 
 app = Flask(__name__)
 #app.config['SECRET_KEY'] = ''
 
+# SocketIO configuration
+async_mode = None
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+
 # Celery configuration
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
-
-# Initialize Celery
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
@@ -19,8 +23,28 @@ celery.conf.update(app.config)
 def index():
     array = [[15.2,2,3],[4,5,6]]
     server_array = [[1,2,3,4,5,6,7,8,9,10,11],[4,5,6,7,8,9,10,11,12,13,11],[4,5,6,7,8,9,10,11,12,13,11]]
+    return render_template('index.html', async_mode=socketio.async_mode,
+                            array=array, server_array=server_array)
 
-    return render_template('index.html', array=array, server_array=server_array)
+def background_thread():
+    count = 0;
+    while True:
+        socketio.sleep(2)
+        count += 1
+        socketio.emit('my_response',
+                      {'data': 'Server generated event', 'count': count},)
+
+@socketio.on('connect')
+def connect():
+    global thread
+    if thread is None:
+        thread = socketio.start_background_task(target=background_thread)
+    emit('my_response', {'data': 'Connected', 'count': 0})
+
+@socketio.on('disconnect')
+def disconnect():
+    print('Client disconnected', request.sid)
 
 if __name__ == '__main__':
     app.run(debug=True)
+    socketio.run(app)
