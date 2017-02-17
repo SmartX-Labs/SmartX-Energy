1b1b4b89eae77a4cb476d74911d32795592675c0
Seungryong Kim <srkim@smartx.kr>
2017-02-17 21:13:18 +0900

    Merging lirc controller

--- 1.IoT/lirc-ac-controller/example.js
+++ 1.IoT/lirc-ac-controller/example.js
@@ -1,7 +1,18 @@
-//How to use it
+var coap = require('coap'),
+    server = coap.createServer();
 
 var lircAcController = require('./lirc-ac-controller.js');
-
 lircAcController.initLircService();
-lircAcController.setPower("ON");
-lircAcController.setTemp("18");
+
+
+// the default CoAP port is 5683
+server.on('request', function(req, res) {
+//	console.log(req);
+    var query = require('url').parse(req.url).query;
+    const queryString = require('query-string').parse(query);
+    console.log(queryString);
+  lircAcController.setTemp(queryString.temperature);
+
+    res.end()
+}).listen();
+
