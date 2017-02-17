bbc870c70f5bf88940123c46f2ea8be9d880b17d
Seungryong Kim <srkim@smartx.kr>
2017-02-17 21:39:25 +0900

    re-re-merging and delete folders

--- 1.IoT/README.md
+++
@@ -1,4 +0,0 @@
-
-해당 코드의 활용을 위한 상세 가이드는 아래의 링크의 자료를 통해 참조하실 수 있습니다.
-
-https://github.com/SmartX-Labs/SmartX-mini/tree/master/Lab-2.%20InterConnect
--- 1.IoT/flume-ng-snmp-source/test/test-flume-snmp-source.sh
+++
@@ -1,1 +0,0 @@
-apache-flume-1.4.0-bin/bin/flume-ng agent -c apache-flume-1.4.0-bin/conf/ -f flume-snmp-source.git/test/resources/flume-snmp-source.conf.example -n agent -Dflume.root.logger=INFO,console
